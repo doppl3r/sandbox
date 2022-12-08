cb91d0dc070c52a6c3ab571449d70c13436ba618
doppl3r <deben3@gmail.com>
2022-12-08 15:12:10 -0700

    Add visual plane buffering

--- src/js/chunk.js
+++ src/js/chunk.js
@@ -16,7 +16,7 @@ class Chunk extends Group {
         }, options);
         
         // Create mesh geometry
-        var geometry = new PlaneGeometry(options.segments, options.segments);
+        var geometry = new PlaneGeometry(options.segments - 1, options.segments - 1, options.segments - 1, options.segments - 1);
         var material = new MeshNormalMaterial({  });
         var plane = new Mesh(geometry, material);
         this.name = 'chunk';
@@ -26,16 +26,22 @@ class Chunk extends Group {
         var prng = new alea('pizza'); // Change parameter to redefine seed 
         var noise = new createNoise4D(prng);
         var scaleNoise = 0.1;
-        var scaleHeight = 1;
+        var scaleHeight = 5;
 
         // Add height to map from z-noise
         var matrix = [];
+        var bufferItemSize = plane.geometry.attributes.position.itemSize;
         for (var x = 0; x < options.segments; x++) {
             matrix.push([]);
             for (var y = 0; y < options.segments; y++) {
                 // noise(x, y, z, w)
+                var index = bufferItemSize * (x * options.segments + y); // Buffer Index
                 var height = noise((x + options.position.x) * scaleNoise, (y + options.position.y) * scaleNoise, 0, 0);
-                matrix[x].push(height * scaleHeight);
+                var z = height * scaleHeight; // Scale
+                plane.geometry.attributes.position.array[index] = x + options.position.x;
+                plane.geometry.attributes.position.array[index + 1] = y + options.position.y;
+                plane.geometry.attributes.position.array[index + 2] = z + options.position.z;
+                matrix[x].push(z);
             }
         }
 
